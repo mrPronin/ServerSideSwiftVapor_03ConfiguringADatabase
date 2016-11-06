ff49be2050bb8a919da7581ecef5d608c0cb5d69
Alexander Pronin <pronin.alx@gmail.com>
2016-11-06 12:32:43 +0200

    Initial commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,17 @@
+import Vapor
+import VaporPostgreSQL
+
+let drop = Droplet(
+    providers: [VaporPostgreSQL.Provider.self]
+)
+
+drop.get("version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    } else {
+        return "No db connection"
+    }
+}
+
+drop.run()
